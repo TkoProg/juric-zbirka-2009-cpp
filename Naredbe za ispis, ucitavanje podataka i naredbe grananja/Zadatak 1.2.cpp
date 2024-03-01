cdb5ffa1c025ff5fb5e70f510e6336dbee479daf
tamiroladejo <koltam09@gmail.com>
2024-03-01 01:43:19 +0100

    Zadatak 1.2 uradjen.

---
+++ Naredbe za ispis, ucitavanje podataka i naredbe grananja/Zadatak 1.2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout<<"Vase ime i prezime\n";
+    std::cout<<"------------------\n";
+    std::cout<<"";
+    std::cout<<"Datum rodjenja\n";
+    std::cout<<"Smjer studija\n";
+    std::cout<<"Grad";
+    return 0;
+}
